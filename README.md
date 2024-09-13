dc781d5bb451832133b636dac5afb05db9d4f34d
jawikas <jawika008@gmail.com>
2024-09-13 09:48:21 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+COMING SOON: SNAPSTER $SNAPS ON OUR GITHUB
+Snapster - Trading App : Snapster Points campaign is live with $SNAPS: Airdrop confirmed! 
+
+Make sure you follow us on our telegram channel and group to get the latest information about this airdrop.
+
+Register : https://t.me/snapster_bot?start=ref_K0S1qpNL2RIGlx
+
+STAY TUNE ON
+- Channel : https://t.me/Deeplchain
+- Group : https://t.me/Deeplchainsup
---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+# This file can be empty, it just indicates that this directory should be treated as a package
+
+COMING SOON: SNAPSTER $SNAPS ON OUR GITHUB
+
+make sure you follow us on our telegram channel and group to get the latest information about this airdrop.
+
+Register : https://t.me/snapster_bot?start=ref_K0S1qpNL2RIGlx
+
+STAY TUNE ON
+- Channel : https://t.me/Deeplchain
+- Group : https://t.me/Deeplchainsup
